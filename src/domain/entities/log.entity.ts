cdbdec28f9751ed3a7787c8c23c3d1c2a75a2ce3
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-09 09:34:37 +0200

    feat: comments to log.entity

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -12,18 +12,30 @@ export interface LogEntityOptions {
   createdAt?: Date;
 }
 
+/**
+ * Class representing a log entity.
+ */
 export class LogEntity {
   public message: string;
   public level: LogSeverityLevel;
   public origin: string;
   public createdAt?: Date;
 
+  /**
+   * Creates an instance of LogEntity.
+   * @param options The options to initialize the log entity.
+   */
   constructor(options: LogEntityOptions) {
     (this.message = options.message), (this.level = options.level);
     this.origin = options.origin;
     this.createdAt = new Date();
   }
 
+  /**
+   * Creates a LogEntity object from a JSON string.
+   * @param json The JSON string representing the log entity.
+   * @returns The created LogEntity object.
+   */
   static fromJson = (json: string): LogEntity => {
     json = json === '' ? '{}' : json;
 
@@ -41,6 +53,11 @@ export class LogEntity {
     return log;
   };
 
+  /**
+   * Creates a LogEntity object from a plain object.
+   * @param object The plain object representing the log entity.
+   * @returns The created LogEntity object.
+   */
   static fromObject = (object: { [key: string]: any }): LogEntity => {
     const { message, level, createdAt, origin } = object;
 
