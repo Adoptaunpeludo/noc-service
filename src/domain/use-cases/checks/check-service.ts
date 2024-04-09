0c0df49d7b1e369038e6703a3e0c8be096d1fb9a
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-09 09:32:32 +0200

    feat: comments to sheck-service

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -2,16 +2,41 @@ import { EmailService } from '../../../presentation/email/email.service';
 import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
 import { LogRepository } from '../../repository/log.repository';
 
+/**
+ * Interface for the use case of checking a service.
+ */
 interface CheckServiceUseCase {
+  /**
+   * Executes the check service operation.
+   * @param url The URL of the service to check.
+   * @returns A promise that resolves to true if the service is reachable, otherwise false.
+   */
   execute(url: string): Promise<boolean>;
 }
 
+/**
+ * Type definition for a success callback function.
+ */
 type SuccessCallback = (() => void) | undefined;
+
+/**
+ * Type definition for an error callback function.
+ */
 type ErrorCallback = ((error: string) => void) | undefined;
 
+/**
+ * Class representing the use case of checking a service.
+ */
 export class CheckService implements CheckServiceUseCase {
   private name = 'check-service.ts';
 
+  /**
+   * Creates an instance of CheckService.
+   * @param emailService The email service used to send notifications.
+   * @param logRepository The repository for saving logs.
+   * @param successCallback Callback function to execute on success.
+   * @param errorCallback Callback function to execute on error.
+   */
   constructor(
     private readonly emailService: EmailService,
     private readonly logRepository: LogRepository,
@@ -19,6 +44,11 @@ export class CheckService implements CheckServiceUseCase {
     private readonly errorCallback: ErrorCallback
   ) {}
 
+  /**
+   * Executes the check service operation.
+   * @param url The URL of the service to check.
+   * @returns A promise that resolves to true if the service is reachable, otherwise false.
+   */
   public async execute(url: string): Promise<boolean> {
     try {
       const req = await fetch(url);
